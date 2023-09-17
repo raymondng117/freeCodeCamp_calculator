65b3ea351555ac28937c00c040f4bca81197cc25
Nywiz <ngyuwing117@gmail.com>
2023-09-17 15:02:17 -0400

    update

--- src/savepoint.js
+++ src/savepoint.js
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
-import Decimal from "decimal.js";
+import Decimal from "decimal.js"; 
 import NumberButtons from "./Buttons";
 
 const Calculator = () => {
-    // ** initialize calc as an object having a combinated state of "num" and "display" 
     let [calc, setCalc] = useState({
         num: "0",
         display: 0,
@@ -15,15 +14,17 @@ const Calculator = () => {
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "."];
     const numbersID = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero", "decimal"];
 
-    const [negative, setNegative] = useState(false)
-    const [result, setResult] = useState(null)
+    const buttonClass = "display-2  btn btn-primary m-2"
 
+    const [negative, setNegative] = useState(false)
 
     const [perviousNum, setPerviousNumber] = useState(null)
     const [currentNum, setCurrentNumber] = useState(null)
 
     const [perviousOperator, setPerviousOperator] = useState("")
     const [currentOperator, setCurrentOperator] = useState("")
+
+    const [result, setResult] = useState(null)
 
 
     function calculate(perviousNum, currentNum, operator) {
@@ -51,79 +52,15 @@ const Calculator = () => {
 
     }
 
-    const numClickHandler = (e) => {
-        e.preventDefault();
-        // capture the number value from button being clicked
-        let value = e.target.innerHTML;
-
-        // check the nagative state, when the state is false which mean wanna positive calc 
-        if (!negative) {
-            // prevStoredNum is an object including num and display!!!! 
-            setCalc(prevStoredNum => ({
-                ...prevStoredNum,
-                // determine the num's state based on conditions
-                num:
-                    // For first number clicking, if the coming number value is not ".", num state should be a number without leading zero (0 => 12)
-                    (prevStoredNum.num === "0" && value !== ".") ? value : // Replace leading zero
-
-                        // For first number clicking,, if the coming number value is 0, num state will ignore additional zeros
-                        (prevStoredNum.num === "0" && value === "0") ? prevStoredNum.num : // Ignore additional zeros
-
-                            // When the prevStoredNum.num (accumulated num) has a "." already, any additional coming "." will be ignored
-                            (value === "." && prevStoredNum.num.includes(".")) ?
-                                prevStoredNum.num : // Ignore additional decimals
-
-                                // Otherwise, keep accumulating the num with every number clicking
-                                prevStoredNum.num + value,
-
-
-                display:
-                    // For first number clicking, if the coming number value is not ".", num state should be a number without leading zero (0 => 12)
-                    (prevStoredNum.num === "0" && value !== ".") ? value : // Replace leading zero
-
-                        // For first number clicking,, if the coming number value is 0, num state will ignore additional zeros
-                        (prevStoredNum.num === "0" && value === "0") ? prevStoredNum.num : // Ignore additional zeros
-
-                            // When the prevStoredNum.num (accumulated num) has a "." already, any additional coming "." will be ignored
-                            (value === "." && prevStoredNum.num.includes(".")) ?
-                                prevStoredNum.num : // Ignore additional decimals
-
-                                // Otherwise, keep accumulating the num with every number clicking
-                                prevStoredNum.num + value,
-            }));
-
-        } else {
-
-            setCalc(prevStoredNum => ({
-                ...prevStoredNum,
-                num:
-                    (prevStoredNum.num === "0" && value !== ".") ? value * -1 : // Replace leading zero
-                        (value === "0" && prevStoredNum.num.toString().startsWith("0")) ?
-                            prevStoredNum.num : // Ignore additional zeros
-                            (value === "." && prevStoredNum.num.includes(".")) ?
-                                prevStoredNum.num : // Ignore additional decimals
-                                (prevStoredNum.num + value) * -1,
-                display:
-                    (prevStoredNum.num === "0" && value !== ".") ? value * -1 : // Replace leading zero
-                        (value === "0" && prevStoredNum.num.toString().startsWith("0")) ?
-                            prevStoredNum.num : // Ignore additional zeros
-                            (value === "." && prevStoredNum.num.includes(".")) ?
-                                prevStoredNum.num : // Ignore additional decimals
-                                (prevStoredNum.num + value) * -1,
-            }));
-        }
-
-    };
-
     const handleOperatorChange = (operatorIndex) => {
-        // every time click a new operator should set back the negative status to be "false"
-        // which assumes every new operator is initially to make positive calc
         setNegative(false)
 
-        // parse the num which is in string format to be float format 
-        let pasredNum = parseFloat(calc.num)
-        let operator = operators[operatorIndex]
+        console.log(calc.num)
 
+        let pasredNum = parseFloat(calc.num)
+        console.log(pasredNum)
+
+        let operator = operators[operatorIndex]
         if (operator !== "AC") {
             if (!perviousOperator) {
                 if (result) {
@@ -210,6 +147,7 @@ const Calculator = () => {
             })
 
         } else {
+            console.log("I DID AC")
             setCalc({
                 num: "0",
                 display: 0
@@ -222,8 +160,46 @@ const Calculator = () => {
         }
     }
 
+    const numClickHandler = (e) => {
+        e.preventDefault();
+        let value = e.target.innerHTML;
+
+        if (!negative) {
+            setCalc(prevStoredNum => ({
+                ...prevStoredNum,
+                num:
+                    (prevStoredNum.num === "0" && value !== ".") ? value : // Replace leading zero
+                        (value === "0" && prevStoredNum.num.toString().startsWith("0")) ? prevStoredNum.num : // Ignore additional zeros
+                            (value === "." && prevStoredNum.num.includes(".")) ? prevStoredNum.num : // Ignore additional decimals
+                                prevStoredNum.num + value,
+                display:
+                    (prevStoredNum.num === "0" && value !== ".") ? value : // Replace leading zero
+                        (value === "0" && prevStoredNum.num.toString().startsWith("0")) ? prevStoredNum.num : // Ignore additional zeros
+                            (value === "." && prevStoredNum.num.includes(".")) ? prevStoredNum.num : // Ignore additional decimals
+                                prevStoredNum.num + value,
+            }));
+        } else {
+            setCalc(prevStoredNum => ({
+                ...prevStoredNum,
+                num:
+                    (prevStoredNum.num === "0" && value !== ".") ? value * -1 : // Replace leading zero
+                        (value === "0" && prevStoredNum.num.toString().startsWith("0")) ? prevStoredNum.num : // Ignore additional zeros
+                            (value === "." && prevStoredNum.num.includes(".")) ? prevStoredNum.num : // Ignore additional decimals
+                                (prevStoredNum.num + value) * -1 ,
+                display:
+                    (prevStoredNum.num === "0" && value !== ".") ? value * -1 : // Replace leading zero
+                        (value === "0" && prevStoredNum.num.toString().startsWith("0")) ? prevStoredNum.num : // Ignore additional zeros
+                            (value === "." && prevStoredNum.num.includes(".")) ? prevStoredNum.num : // Ignore additional decimals
+                                (prevStoredNum.num + value) * -1,
+            }));
+        }
+
+    };
+
+
     // after pre-setting all data state, use them only within useEffect, and then you can set their state back
     // result only can be processed in *useEffect* untill all factors are set before the useEffect
+
     useEffect(() => {
         if (negative) {
             if (!currentNum) {
@@ -232,7 +208,9 @@ const Calculator = () => {
                 setCurrentOperator("-")
                 setNegative(false)
             }
+
         }
+
 
         if (perviousNum && currentNum && perviousOperator && currentOperator && !result) {
             if (currentOperator === "=") {
@@ -288,7 +266,7 @@ const Calculator = () => {
                     </div>
                 </div>
             </div>
-
+    
             <div className="row" id="panel">
                 <div className="col-12">
                     <div className="d-flex justify-content-center align-items-center flex-wrap">
@@ -298,13 +276,7 @@ const Calculator = () => {
                     </div>
                 </div>
             </div>
-
-            {/* 
-            create each number button with key, id and most important, the numberClicker 
-            notes for onclick:
-             1. when the callback function has no parameters, just call the function with out ();
-             2. otherwise, onClick = {() => numClickHandler(parameter1, ...)}
-            */}
+    
             <div className="row" id="panel">
                 <div className="col-12">
                     <div className="d-flex justify-content-center align-items-center flex-wrap">
@@ -316,7 +288,7 @@ const Calculator = () => {
             </div>
         </div>
     );
-
+    
 }
 
 export default Calculator;
